b0ba8376f9b98f1f5d40cf0f861a86d58d40ff68
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-08 16:58:00 -0600

    commit tree c2ee6c755475b80104693421a84067e34dc2b629

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    def initialize(string)
+        @string = string
+    end 
+
+    def get_response_body
+        #sends a GET req to the url passed in on initial.
+        #return the body of the response
+        uri = URI.parse(@string) 
+        # pass the srt. arg
+        response = Net::HTTP.get_response(uri) # assigns response vari. 
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body) # uses met. to send a req. 
+        # return a Ruby array or hash made up of data converted from the resp. req.
+    end
+
+end
